ed5313e3fe62023e9ec3f533c7a0975d5ae0d7a9
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2021-03-26 00:45:51 -0600

    Readding project files

--- ClientApp/src/main/java/com/daxthompsonproject1/clientapp/HomeActivity.java
+++ ClientApp/src/main/java/com/daxthompsonproject1/clientapp/HomeActivity.java
@@ -1,14 +1,71 @@
 package com.daxthompsonproject1.clientapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
 
-public class HomeActivity extends AppCompatActivity {
+import com.daxthompsonproject1.viewmodels.models.Reservation;
+import com.daxthompsonproject1.viewmodels.viewmodels.ClientViewModel;
+
+public class HomeActivity extends AppCompatActivity{
+
+    private ClientViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        this.viewModel = new ViewModelProvider(this).get(ClientViewModel.class);
+
+        AppCompatTextView currentUser = findViewById(R.id.currentUser);
+
+        viewModel.getUser().observe(this, (user) -> {
+            if(user == null){
+                currentUser.setText("Loading...");
+                Intent intent = new Intent(this, SignInActivity.class);
+                startActivity(intent);
+            }
+            else{
+                currentUser.setText(viewModel.getUserIdentity());
+            }
+        });
+
+//        AppCompatButton logOut = findViewById(R.id.logOut);
+//        logOut.setOnClickListener((view) -> {
+//            viewModel.signOut();
+//        });
+//
+//        AppCompatTextView managerData = findViewById(R.id.managerData);
+//        this.viewModel.getManagerData().observe(this, (data) -> {
+//            if(this.viewModel.getManagerData().getValue() == null){
+//                managerData.setText("Loading...");
+//            }
+//            else {
+//                managerData.setText(this.viewModel.getManagerData().getValue().toString());
+//            }
+//        });
+//
+//        findViewById(R.id.makeReservation).setOnClickListener(view -> {
+//            this.viewModel.makeReservation();
+//        });
+//
+//        LinearLayout reservationContainer = findViewById(R.id.reservationContiner);
+//        this.viewModel.getWaitList().observe(this, (view) -> {
+//            reservationContainer.removeAllViews();
+//
+//            Log.d("HOMEACTIVITY-UPDATEWAITLIST", "Waitlist was changed");
+//            StringBuilder reservations = new StringBuilder();
+//            for(Reservation reservation : this.viewModel.getWaitList().getValue()){
+//                reservationContainer.addView(this.viewModel.renderReservation(reservation, this));
+//            }
+//        });
+
     }
 }
